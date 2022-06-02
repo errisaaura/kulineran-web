33f947a8a97815322bdadf5c0c33a1ccc7415a1b
errisaaura <errisahidayah@gmail.com>
2022-06-02 16:47:37 +0700

    Page Home

---
+++ src/components/Hero.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hero">
+    <!-- dekstop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <!-- <h2><strong>Delicious Food Menu, </strong> <br> in Your Gadget</h2> -->
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong> <br />
+                in Your Gadget
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorit anda</p>
+              <!-- <router-link to="/" class="btn btn-lg btn-success">Pesan</router-link> -->
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right>Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/Image/Hero.png" width="100%" />
+        </div>
+      </div>
+    </div>
+
+    <!-- mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+           <div class="col-md-6 mb-3">
+          <img src="../assets/Image/Hero.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <!-- <h2><strong>Delicious Food Menu, </strong> <br> in Your Gadget</h2> -->
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong> <br />
+                in Your Gadget
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorit anda</p>
+              <!-- <router-link to="/" class="btn btn-lg btn-success">Pesan</router-link> -->
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right>Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroHome",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <div class="container">
+          <b-navbar-brand href="#">Kulineran</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+            <router-link class="nav-link" to="/" >Home</router-link>
+            <router-link class="nav-link" to="/foods">Foods</router-link>
+         
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <router-link class="nav-link" to="/keranjang">Keranjang
+                <b-icon-bag></b-icon-bag>
+                <span class="badge badge-success ml-2">0</span>
+            </router-link>
+
+        </b-navbar-nav>
+      </b-collapse>
+
+      </div>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarMenu",
+};
+</script>
+
+<style>
+</style>
